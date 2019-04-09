a00a06cb55830ed9f5a8a60641caf225a3e3848f
Anshuman Sanghvi <anshumansanghvi@gmail.com>
2019-04-10 03:02:10 +0530

    added Widget pojo class. This will be the object handled by lambda function.

---
+++ src/main/java/com/anshumansanghvi/widget/model/Widget.java
@@ -0,0 +1,17 @@
+package com.anshumansanghvi.widget.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Widget {
+    private String id;
+    private String name;
+}
