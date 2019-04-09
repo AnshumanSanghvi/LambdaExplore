1d916808ff015814b03e6dc4e0d6e9a3959d7776
Anshuman Sanghvi <anshumansanghvi@gmail.com>
2019-04-10 03:03:48 +0530

    created widget request pojo class with getters and setters for creating id.

---
+++ src/main/java/com/anshumansanghvi/widget/model/WidgetRequest.java
@@ -0,0 +1,15 @@
+package com.anshumansanghvi.widget.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class WidgetRequest {
+    private String id;
+
+}
